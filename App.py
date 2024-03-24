4d2d1e2c984c900f2dcaf20daf2de7c379c4ec27
mimigarci <michelle.garcia@correo.unimet.edu.ve>
2024-03-24 19:51:55 -0400

    Avance 1
    mini avance
    + construcción de clases

--- App.py
+++ App.py
@@ -1,10 +1,34 @@
-
+from Clases.Leader import Leader
+from Clases.Player import Player
+from Clases.Trainer import Trainer
+from Clases.Town import Town
+from Clases.Route import Route
+from Clases.Pokemon import Pokemon
 class App:
     
     def __init__(self):
         self.pokemons = []
         self.locations = []
         self.trainers = []
+
+    def main_data (self):
+        
+        #Pokemons:
+        
+        #Pueblos:
+
+        #Líderes:
+
+        #Compañeros:
+
+
+        #Dump info in pickle
+        pass
+
+
+    #Se debería de activar después de cada batalla
+    def save_data(self):
+        pass
 
     def menu (self):
         pass
