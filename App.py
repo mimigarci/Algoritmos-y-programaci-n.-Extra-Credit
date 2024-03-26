cc877d897098e297b748d1466e5f234088e43418
mimigarci <michelle.garcia@correo.unimet.edu.ve>
2024-03-26 16:46:53 -0400

    Update App.py

--- App.py
+++ App.py
@@ -10,6 +10,7 @@ class App:
         self.pokemons = []
         self.locations = []
         self.trainers = []
+        self.player = ""
 
     def main_data (self):
         
@@ -33,5 +34,124 @@ class App:
     def menu (self):
         pass
 
+<<<<<<< Updated upstream
     def start (self):
         App.menu(self)
+=======
+    1. Empezar una partida nueva
+    2. Cargar partida
+    3. Salir                        
+
+    ---> """)
+            if option == "1":
+
+                select = input("""
+        1. Guardado 1
+        2. Guardado 2
+        3. Guardado 3
+            
+            ---> """)
+                
+                if select == "1":
+                    App.start_game(self, "Db\Save1.pickle")
+                elif select == "2":
+                    App.start_game(self, "Db\Save2.pickle")
+                elif select == "3":
+                    App.start_game(self, "Db\Save3.pickle")
+                else:
+                    print ("Opción inválida")
+
+            elif option == "2":
+                
+                select = input("""
+        1. Guardado 1
+        2. Guardado 2
+        3. Guardado 3
+            
+            ---> """)
+                
+                if select == "1":
+                    App.read_data(self, "Db\Save1.pickle")
+                    App.start_game(self, "Db\Save1.pickle")
+                    
+                elif select == "2":
+                    App.read_data(self, "Db\Save2.pickle")
+                    App.start_game(self, "Db\Save2.pickle")
+                    
+                elif select == "3":
+                    App.read_data(self, "Db\Save3.pickle")
+                    App.start_game(self, "Db\Save3.pickle")
+                    
+                else:
+                    print ("\nOpción inválida\n")
+                
+
+            elif option == "3":
+                break
+            else:
+                print ("Opción inválida")
+
+
+    def start_game(self, file_name):
+        print ("Bienvenido a tu aventura pokemon! ")
+
+        region = input ("""De qué región eres?
+    
+    1. Barinas
+    2. Barinas
+    3. Barinas
+        
+        ---> """)
+
+        if region == "1":
+            companion = self.trainers[0]
+            starter_pokemon = input ("""Seleccione un pokemon:
+        1.
+        2.
+        3.
+        
+            ---> """)
+        elif region == "2":
+            companion = self.trainers[1]
+            starter_pokemon = input ("""Seleccione un pokemon:
+        1.
+        2.
+        3.
+        
+            ---> """)
+        elif region == "3":
+            companion = self.trainers[2]
+            starter_pokemon = input ("""Seleccione un pokemon:
+        1.
+        2.
+        3.
+        
+            ---> """)
+
+        else:
+            print ("\nOpción inválida\n")
+
+
+        starter_pokemon = input ("")
+
+
+    def get_location (self):
+        pass
+
+    def unlocked_towns_menu (self):
+        unlocked_towns = []
+        registered_locations = len(self.locations)
+
+        if registered_locations > 0:
+            for i in self.locations:
+                registered_locations -= 1
+                if type(i) == Town:
+                    if i.battle_cleared == True:
+                        unlocked_towns.append(i)
+                    else:
+                        continue
+                else:
+                    continue
+        else:
+            print ("No hay pueblos desbloqueados")
+>>>>>>> Stashed changes
