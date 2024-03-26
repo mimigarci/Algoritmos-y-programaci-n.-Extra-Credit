4dd19c8f5ffc7ae976a9da37ad9e0abc02151171
L-Asenjo <156253241+L-Asenjo@users.noreply.github.com>
2024-03-26 18:00:52 -0400

    Definiendo clase movimentos

---
+++ Clases/Movement.py
@@ -0,0 +1,7 @@
+
+class Movement:
+  def __init__(self, name, description, power, effects):
+    self.name = name
+    self.description = description
+    self.power = power
+    self.effects = effects
