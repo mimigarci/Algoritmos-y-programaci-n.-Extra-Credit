51fea4098beb0af5b8943b9874ca75788bfd0ad1
L-Asenjo <156253241+L-Asenjo@users.noreply.github.com>
2024-03-27 15:49:22 -0400

    Fixed encounter

--- Clases/Route.py
+++ Clases/Route.py
@@ -10,18 +10,19 @@ class Route(Location):
         self.wild_pokemon = wild_pokemon
 
     def encounter(self, player):
-        if self.number in range(1, 5):
-            self.battle_cleared = Battle.battle_pokemon(player, self.wild_pokemon)
-        elif self.number in range(5, 9):
-            aux = random.randrange(4)
-            if aux in range(3):
+        if self.battle_cleared == False:
+            if self.number in range(1, 5):
                 self.battle_cleared = Battle.battle_pokemon(player, self.wild_pokemon)
-        elif self.numer in range(9, 13)
-            aux = random.randrange(2)
-            if aux == 0:
-                self.battle_cleared = Battle.battle_pokemon(player, self.wild_pokemon)
-        else:
-            aux = random.randrange(100)
-            if aux in range(2):
-                self.battle_cleared = Battle.battle_pokemon(player, self.wild_pokemon)
+            elif self.number in range(5, 9):
+                aux = random.randrange(4)
+                if aux in range(3):
+                    self.battle_cleared = Battle.battle_pokemon(player, self.wild_pokemon)
+            elif self.numer in range(9, 13)
+                aux = random.randrange(2)
+                if aux == 0:
+                    self.battle_cleared = Battle.battle_pokemon(player, self.wild_pokemon)
+            else:
+                aux = random.randrange(100)
+                if aux in range(2):
+                    self.battle_cleared = Battle.battle_pokemon(player, self.wild_pokemon)
                 
