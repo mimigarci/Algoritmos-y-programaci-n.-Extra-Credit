8a32aea992c69059294416a0ab911a2f9a6f090d
L-Asenjo <156253241+L-Asenjo@users.noreply.github.com>
2024-03-27 12:36:48 -0400

    Definiendo clase auxiliar funciones

---
+++ Clases/Functions.py
@@ -0,0 +1,12 @@
+class Functions:
+  def __init__(self):
+    pass
+
+  def manage_options(options):
+    for i in range(len(options)):
+        print(f"{i+1}. {options[i]}")
+    option = input("Ingrese el número de su opción: ")
+    while not option.isnumeric() or int(option)-1 not in range(len(options)):
+        option = input("Ingreso inválidos, ingrese el número de su opción: ")
+    
+    return int(option)
