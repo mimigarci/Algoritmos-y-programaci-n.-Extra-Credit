3eab4c5c4ef16a146731cf90d264be128796139b
mimigarci <156366795+mimigarci@users.noreply.github.com>
2024-03-28 20:12:06 -0400

    Delete Clases directory

--- Clases/Leader.py
+++
@@ -1,9 +0,0 @@
-from Clases.Trainer import Trainer
-
-class Leader(Trainer):
-    
-    def __init__(self, name, age, town, p1, p2):
-        super().__init__(name, age)
-
-        self.town = town
-        self.pokemons = [p1, p2]
--- Clases/Trainer.py
+++
@@ -1,9 +0,0 @@
-class Trainer:
-    
-    def __init__(self, name, age):
-        
-        self.name = name
-        self.age = age
-        self.pokemons = []
-
-    
